c8a6e789904e7537dcb9a34b0656aa2c8737bae9
Hotu Nga Morehu Elkington <hotungamorehuelkington@ma388.home>
2021-08-02 15:27:49 +0100

    Added #top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,9 +4,11 @@ describe Oystercard do
   before do
     @card1 = Oystercard.new
   end
-  
+
   it 'checks that a card has an initial balance of 0' do
     expect(@card1.balance).to eq(0)
   end
+
+  it { is_expected.to respond_to(:top_up) }  
   
 end
