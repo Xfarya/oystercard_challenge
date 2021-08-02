6fc984c19218b43dfa3a7b5ee0e48ebc699b007b
William <williamjday11@gmail.com>
2021-08-02 14:18:32 +0000

    Added first test to check balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,12 @@
 require 'oystercard'
 
 describe Oystercard do
-
+  before do
+    @card1 = Oystercard.new
+  end
+  
+  it 'checks that a card has an initial balance of 0' do
+    expect(@card1.balance).to eq(0)
+  end
+  
 end
